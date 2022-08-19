2e9bbcda29897fbfac5b14fda862c6acaba6dc15
Melvin7878 <melvin.turessonnilsson@elev.ga.lbs.se>
2022-08-19 15:06:46 +0200

    Project created

---
+++ Repetition Programmering 2/Repetition Programmering 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repetition_Programmering_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Assignment 1
+
+            //Heltal
+            Console.Write("Please enter a number without decimals into the console");
+           private string numberUserInput = Console.ReadLine();
+        }
+    }
+
+}
